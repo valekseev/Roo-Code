92e53ef978c730d639f6f0fd778b8d77fa1c9fcc
Matt Rubens <mrubens@users.noreply.github.com>
2025-06-17 07:35:20 -0400

    MDM check improvements (#4770)
    * Better check for production clerk
    
    * Send to account tab if required
    
    * Fix tests

--- src/core/webview/ClineProvider.ts
+++ src/core/webview/ClineProvider.ts
@@ -556,11 +556,6 @@ export class ClineProvider
 			>
 		> = {},
 	) {
-		// Check MDM compliance before proceeding
-		if (!this.checkMdmCompliance()) {
-			return // Block task creation if not compliant
-		}
-
 		const {
 			apiConfiguration,
 			organizationAllowList,
@@ -1280,6 +1275,11 @@ export class ClineProvider
 
 		// Update VSCode context for experiments
 		await this.updateVSCodeContext()
+
+		// Check MDM compliance and send user to account tab if not compliant
+		if (!this.checkMdmCompliance()) {
+			await this.postMessageToWebview({ type: "action", action: "accountButtonClicked" })
+		}
 	}
 
 	/**
@@ -1494,6 +1494,7 @@ export class ClineProvider
 				codebaseIndexEmbedderBaseUrl: "",
 				codebaseIndexEmbedderModelId: "",
 			},
+			mdmCompliant: this.checkMdmCompliance(),
 		}
 	}
 
@@ -1749,11 +1750,6 @@ export class ClineProvider
 		const compliance = this.mdmService.isCompliant()
 
 		if (!compliance.compliant) {
-			vscode.window.showErrorMessage(compliance.reason, "Sign In").then((selection) => {
-				if (selection === "Sign In") {
-					this.postMessageToWebview({ type: "action", action: "accountButtonClicked" })
-				}
-			})
 			return false
 		}
 
